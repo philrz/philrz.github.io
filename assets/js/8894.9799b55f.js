d6424f0e1b0e41c64e2f1d354afb418f29f5450b
Phil Rzewski <phil.rzewski@sysdig.com>
2022-08-14 10:40:08 -0700

    Just website index

--- assets/js/8894.9799b55f.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkzed_docs=self.webpackChunkzed_docs||[]).push([[8894],{8894:function(e,s,c){c.r(s)}}]);
--- assets/js/b2f554cd.a2ba6275.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkzed_docs=self.webpackChunkzed_docs||[]).push([[1477],{10:function(s){s.exports={blogPosts:[]}}}]);
